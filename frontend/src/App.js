afe1d86333e5939cb48d7c08e0c708d131923b0b
SuyashKejriwal <suyashkejriwal1308@gmail.com>
2021-01-18 16:52:38 +0530

    react setup commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>App</h1>
+    </Fragment>
+      );
+}
+
+export default App;
